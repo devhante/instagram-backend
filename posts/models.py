b09b266fed8f038a297f0528ce2ac7d1d05e4047
kyungwook <ironside2001@naver.com>
2019-02-19 14:20:21 +0900

    [Add] date 필드 변경

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class User(models.Model):
     username = models.CharField(max_length=50, primary_key=True)
@@ -9,7 +10,7 @@ class Post(models.Model):
     id = models.AutoField(primary_key=True)
     writer = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     content = models.CharField(max_length=2200)
-    date = models.DateField(auto_now_add=True, blank=True)
+    date = models.DateField(default=timezone.now)
     favor = models.BooleanField(default=False)
 
 
@@ -25,7 +26,7 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     index = models.IntegerField(default=0)
     content = models.CharField(max_length=2200)
-    date = models.DateField(auto_now_add=True, blank=True)
+    date = models.DateField(default=timezone.now)
 
 
 class Reply(models.Model):
@@ -33,4 +34,4 @@ class Reply(models.Model):
     comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
     index = models.IntegerField(default=0)
     content = models.CharField(max_length=2200)
-    date = models.DateField(auto_now_add=True, blank=True)
+    date = models.DateField(default=timezone.now)
